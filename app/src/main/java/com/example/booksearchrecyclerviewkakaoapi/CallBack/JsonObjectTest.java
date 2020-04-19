4402caa789d4e94de7ea028bd4ac719a53094163
hyunho058 <lhyun058@gmail.com>
2020-04-20 00:17:09 +0900

    Update Retrofit2

--- app/src/main/java/com/example/booksearchrecyclerviewkakaoapi/CallBack/JsonObjectTest.java
+++ app/src/main/java/com/example/booksearchrecyclerviewkakaoapi/CallBack/JsonObjectTest.java
@@ -4,6 +4,7 @@ import android.os.AsyncTask;
 import android.util.Log;
 
 import com.example.booksearchrecyclerviewkakaoapi.model.Document;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -13,6 +14,9 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * AsyncTask<시작파라미터,진행상태,서버로받은데이터를 리턴할때사용하는타입>
@@ -79,6 +83,21 @@ public class JsonObjectTest extends AsyncTask<String, Void, ArrayList<Document>>
         } catch (Exception e) {
             Log.v(TAG, "run()_Exception==" + e.toString());
         }
+
+
+//        Retrofit client = new Retrofit.Builder().baseUrl(
+//                "https://dapi.kakao.com").addCallAdapterFactory(GsonConverterFactory.create()).build();
+
+        /**
+         *  https://falinrush.tistory.com/5
+         *  https://kang6264.tistory.com/entry/Retrofit-%EA%B8%B0%EB%B3%B8-%EA%B8%B0%EB%8A%A5%EC%97%90-%EB%8C%80%ED%95%B4%EC%84%9C-%EC%95%8C%EC%95%84%EB%B3%B4%EC%9E%90%EB%82%A0%EC%94%A8%EB%A5%BC-%EC%A1%B0%ED%9A%8C%ED%95%98%EB%8A%94-RestAPI
+         *   https://altongmon.tistory.com/745
+         *   https://calyfactory.github.io/view-binding/
+         *   https://square.github.io/retrofit/
+         */
+
+
+
         return documentList;
     }
 }
--- app/src/main/java/com/example/booksearchrecyclerviewkakaoapi/CallBack/RetrofitService.java
+++ app/src/main/java/com/example/booksearchrecyclerviewkakaoapi/CallBack/RetrofitService.java
@@ -2,20 +2,14 @@ package com.example.booksearchrecyclerviewkakaoapi.CallBack;
 
 import com.example.booksearchrecyclerviewkakaoapi.model.Document;
 
-import java.util.List;
-
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Headers;
 import retrofit2.http.Query;
 
+/**
+ *  KaokaoAPI OookSearch URL = "https://dapi.kakao.com/v3/search/book?target=title"
+ */
 public interface RetrofitService {
-    String url = "https://dapi.kakao.com/v3/search/book?target=title";
-    @Headers("Authorization : KakaoAK a85301089026f3d76b61ac72f59b1d91")
-    @GET("/v3/search/book?target=title")
-
-
-    Call<List<Document>> getData2(@Query("target") String title);
-
-
+    @GET("/v3/search/book")
+    Call<Document> deocument(@Query("target") String keyword);
 }
